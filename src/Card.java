3098b35ad1513a406f2905530f4c6bb9a45c4c89
Samuel Zhou <szhou198@student.fuhsd.org>
2021-05-06 10:46:13 -0700

    update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,26 @@
 
 public class Card {
+	private String rank;
+	private String suit;
+	
+	Card(String rank, String suit){
+		this.rank = rank;
+		this.suit = suit;
+	}
+	
+	public boolean isNine() {
+		if(rank == "NINE") return true;
+		return false;
+	}
+	
+	public String getRank() {
+		return rank;
+	}
+	
+	public String getSuit() {
+		return suit;
+	}
+	
+	
 	
 }
--- src/Deck.java
+++ src/Deck.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 public class Deck{
 	private ArrayList<Card> cards;
 	
-	
 	public Deck() {
 		cards = new ArrayList<Card>();
 	}

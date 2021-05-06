f1430158320071a4c6693da0dc9cf2d390007a95
Samuel Zhou <szhou198@student.fuhsd.org>
2021-05-06 10:59:27 -0700

    update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Server server = new Server();
-		server.run();
+//		Server server = new Server();
+//		server.run();
+		
+		JFrame frame = new JFrame("Welcome Screen");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setSize(500,400);
+		frame.getContentPane().setBackground(new Color(0, 0, 0));
 	}
 
 }

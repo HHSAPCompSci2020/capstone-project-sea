2a2cafcefd3e51ecfd7392fe66dec3d216678aa5
Samuel Zhou <szhou198@student.fuhsd.org>
2021-05-06 10:26:46 -0700

    Update Deck

--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
 
-public class Deck {
-
+public class Deck{
+	private ArrayList<Card> cards;
+	
+	
+	public Deck() {
+		cards = new ArrayList<Card>();
+	}
+	
+	public ArrayList<Card> getDeck(){
+		return cards;
+	}
+	
+	public Card getCard(int index) {
+		return cards.get(index);
+	}
+	
+	public void addCard(Card card) {
+		cards.add(card);
+	}
+	
+	
+	
+	
 }

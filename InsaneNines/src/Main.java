9062ccdd133db525ffccb8262cd1f75a49db389c
sding504 <sding504@student.fuhsd.org>
2021-05-09 13:43:41 -0700

    menu screen

--- InsaneNines/src/Main.java
+++ InsaneNines/src/Main.java
@@ -1,50 +1,101 @@
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Main extends JPanel{
+public class Main implements ActionListener{
 
-	public static String password;
-	
-	public static void main(String[] args) {
-//		Server server = new Server();
-//		server.run();
+	public static final int GAME_SIZE = 750;
+
+	private JFrame f;
+	private JPanel menu, waitRoom, game;
+	private JButton createServer, joinServer, tutorial, back;
+	private JLabel l;
+
+	public Main() {
+		//Server server = new Server();
+		//server.run();
+
+		f = new JFrame("Welcome Screen");
+		createServer = new JButton("Create Server");
+		joinServer = new JButton("Join Server");
+		tutorial = new JButton("Tutorial");
+		back = new JButton("Back");
+		menu = new JPanel();
+		waitRoom = new JPanel();
+		game = new JPanel();
+		l = new JLabel("Waiting for players... Player 1 (you)");
+
 		
-		JFrame frame = new JFrame("Welcome Screen");
+		menu.setLayout(null);
+		createServer.setBounds(40, 80, 400, 40);
+		joinServer.setBounds(40, 140, 400, 40);
+		tutorial.setBounds(40, 200, 400, 40);
+		menu.add(createServer);
+		menu.add(joinServer);
+		menu.add(tutorial);
+		menu.setBackground(Color.WHITE);
+		f.add(menu);
 		
-		JButton button = new JButton("Start");
-		button.addActionListener(new ActionListener() {
+		
+		waitRoom.setLayout(null);
+		back.setBounds(350, 300, 100, 40);
+		l.setBounds(40, 200, 400, 40);
+		//update player count in the label later
+		waitRoom.add(back);
+		waitRoom.add(l);
+		waitRoom.setBackground(Color.WHITE);
+		
+
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+		f.setSize(500,400);
+
+
+		createServer.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				password = JOptionPane.showInputDialog("Enter Room Key");
+				f.setContentPane(waitRoom);
+				f.invalidate();
+				f.validate();
 			}
-		}
-		
-		);
-		
-		JPanel panel = new JPanel();
-		panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 100, 300));
-		panel.setLayout(new GridLayout(0, 1));
-		panel.add(button);
-			
-		frame.add(panel, BorderLayout.CENTER);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("Welcome Screen");	
-		frame.pack();
-		frame.setVisible(true);
-		
+		});
+
+		joinServer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+
+		tutorial.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+
+		back.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				f.setContentPane(menu);
+				f.invalidate();
+				f.validate();
+			}
+		});
+
+
+
+
 	}
-	
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		new Main();
+
+	}
+
+
 
 }
+
